944d3bfc849f45af24dbdd01d0959b3f93eaff4e
yusuke.matsuda <yusuke.matsuda@172.16.2.102>
2024-05-21 16:56:37 +0900

    5/21

---
+++ src/main/java/com/example/demo/News.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+public class News {
+	private String title;
+	private String descrivation;
+	private String url;
+	private int urlToImage;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescrivationString() {
+		return descrivation;
+	}
+	public void setDescrivationString(String descrivationString) {
+		this.descrivation = descrivationString;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public  int getUrlToImageString() {
+		return urlToImage;
+	}
+	public void setUrlToImageString(int urlToImageString) {
+		this.urlToImage = urlToImageString;
+	}
+	
+	
+}
